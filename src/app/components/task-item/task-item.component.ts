59e11c02906326577153adce5cf4a3e05f52c116
Salomeleshka <salomeleshka@gmail.com>
2022-11-22 19:06:37 +0100

    start task-tracker app

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css'],
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onDelete(task: any) {
+    this.onDeleteTask.emit(task);
+  }
+}
